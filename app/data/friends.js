b9fe1e8e6bbaf80949030c2faf4a05446276707c
Jerne <Surajpatel527@gmail.com>
2019-06-12 14:36:28 -0400

    Changelogs: friends.js completed

---
+++ app/data/friends.js
@@ -0,0 +1,29 @@
+var friends = [
+	{
+		name: "Fill Murray",
+		photo: "https://www.fillmurray.com/640/360",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Sarah the Cat",
+		photo: "https://placekitten.com/g/200/300",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Steven",
+		photo: "https://www.stevensegallery.com/640/360",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Molly the Zombie",
+		photo: "https://placezombie.com/640x360",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Nicholas Cage",
+		photo: "https://www.placecage.com/640/360",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
